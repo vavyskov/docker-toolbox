af8bf5de16b09f483d0c404163e1a4b6325ae5d5
Tomáš Daněk <danekt@centrum.cz>
2020-04-17 08:34:43 +0200

    HW - memory

--- scripts/swarm-cluster-setup.sh
+++ scripts/swarm-cluster-setup.sh
@@ -4,7 +4,9 @@
 ## Create a Swarm cluster with 3 nodes (1 Manager & 2 Workers)
 
 for i in 1 2 3; do
-  docker-machine create -d virtualbox node-$i
+    docker-machine create -d virtualbox --virtualbox-memory 1024 node-$i
+    #docker-machine create -d virtualbox --virtualbox-memory 4096 --virtualbox-cpu-count 2 --virtualbox-disk-size 50000 node-$i
+
 done
 
 eval "$(docker-machine env node-1)"
@@ -14,8 +16,8 @@ docker swarm init --advertise-addr "$(docker-machine ip node-1)"
 TOKEN=$(docker swarm join-token -q worker)
 
 for i in 2 3; do
-  eval "$(docker-machine env node-$i)"
-  docker swarm join --token $TOKEN "$(docker-machine ip node-1)":2377
+    eval "$(docker-machine env node-$i)"
+    docker swarm join --token $TOKEN "$(docker-machine ip node-1)":2377
 done
 
 echo "Swarm cluster has been successfuly created !";
